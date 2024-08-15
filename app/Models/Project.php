14f62ae7248364f7aebdb462329425fcc5190e55
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 21:50:00 +0300

    Added necessary fields and relations to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,45 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'department_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string',
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    /**
+     * Get the department associated with the project.
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    /**
+     * Get the employees assigned to the project.
+     */
+    public function employees()
+    {
+        return $this->belongsToMany(Employee::class, 'employee_projects');
+    }
 }
