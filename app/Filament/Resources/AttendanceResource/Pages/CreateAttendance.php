0d7dcb93d9b5e745b1fcb55df0e329ba68401942
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 23:36:56 +0300

    Added on attendance create notifications

--- app/Filament/Resources/AttendanceResource/Pages/CreateAttendance.php
+++ app/Filament/Resources/AttendanceResource/Pages/CreateAttendance.php
@@ -5,8 +5,22 @@ namespace App\Filament\Resources\AttendanceResource\Pages;
 use App\Filament\Resources\AttendanceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateAttendance extends CreateRecord
 {
     protected static string $resource = AttendanceResource::class;
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Attendance Created';
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Attendance Created')
+            ->body('The attendance has been created successfully.');
+    }
 }
