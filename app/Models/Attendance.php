4d95461cb64d9660fca5494b26c12fec5245a7cb
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 23:11:00 +0300

    Added fields to attendance modell

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,4 +8,58 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'employee_id',
+        'date',
+        'check_in',
+        'check_out',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date' => 'date',
+        'check_in' => 'datetime:H:i',
+        'check_out' => 'datetime:H:i',
+    ];
+
+    /**
+     * Get the employee associated with the attendance record.
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    /**
+     * Determine if the employee was present on the given date.
+     *
+     * @return bool
+     */
+    public function wasPresent()
+    {
+        return !is_null($this->check_in);
+    }
+
+    /**
+     * Calculate the total hours worked.
+     *
+     * @return float|null
+     */
+    public function totalHoursWorked()
+    {
+        if ($this->check_in && $this->check_out) {
+            return $this->check_out->diffInHours($this->check_in);
+        }
+        return null;
+    }
 }
