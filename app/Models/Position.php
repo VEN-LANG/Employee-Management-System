0f8dae9b4aa447694334aae2839d6a2f04947af1
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 20:57:21 +0300

    Added fillables and relationships

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'min_salary',
+        'max_salary',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'title' => 'string',
+        'min_salary' => 'decimal:2',
+        'max_salary' => 'decimal:2',
+    ];
+
+    /**
+     * Get the employees that hold this position.
+     */
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
