bde79220ea789299774746656f2c2b912a791a83
venomous-maker <morganokumu8+1@gmail.com>
2024-08-16 02:16:09 +0300

    Updated fields and relations

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -8,4 +8,49 @@ use Illuminate\Database\Eloquent\Model;
 class Leave extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'employee_id',
+        'type',
+        'start_date',
+        'end_date',
+        'reason',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'status' => 'string',
+    ];
+
+    /**
+     * Get the employee that the leave is associated with.
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    /**
+     * Scope a query to only include leaves of a given status.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $status
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfStatus($query, $status)
+    {
+        return $query->where('status', $status);
+    }
 }
