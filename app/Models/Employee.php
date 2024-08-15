ab8711e15f93f33f7b58c42a72eb7525da00c29f
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 20:48:16 +0300

    Finalised on employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,55 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'phone_number',
+        'position_id', // New position_id field
+        'salary',
+        'date_of_joining',
+        'department_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'user_id' => 'integer',
+        'position_id' => 'integer', // Cast position_id to integer
+        'salary' => 'decimal:2',
+        'date_of_joining' => 'date',
+        'department_id' => 'integer',
+    ];
+
+    /**
+     * Get the user associated with the employee.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the department associated with the employee.
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    /**
+     * Get the position associated with the employee.
+     */
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
 }
