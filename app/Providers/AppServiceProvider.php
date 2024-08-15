6d8dd1639cc1dbd787a5a0817feb17a93237ab9b
venomous-maker <morganokumu8+1@gmail.com>
2024-08-15 23:06:52 +0300

    Introduced multilingual support to the application

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\Enums\Placement;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en','fr','sw'])
+                ->outsidePanelPlacement(Placement::BottomRight); // also accepts a closure
+        });
     }
 }
