4fe5cbdf788d4090ab1f2bfa9107ab1793eddd11
venomous-maker <morganokumu8+1@gmail.com>
2024-08-16 11:57:21 +0300

    Added admin dashboard class

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Resources\UserResource\Widgets\UsersOverview;
+use Filament\Pages\Page;
+
+class Dashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected static string $view = 'filament.pages.dashboard';
+
+    public function getWidgets(): array
+    {
+        return [
+            UsersOverview::class,
+        ];
+    }
+}
