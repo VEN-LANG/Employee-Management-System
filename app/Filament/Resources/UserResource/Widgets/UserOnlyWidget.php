3154d1248b4259498e78c9c1b0e5ab41bbe59872
venomous-maker <morganokumu8+1@gmail.com>
2024-08-16 23:03:16 +0300

    Added extra stats

--- app/Filament/Resources/UserResource/Widgets/UserOnlyWidget.php
+++ app/Filament/Resources/UserResource/Widgets/UserOnlyWidget.php
@@ -15,11 +15,12 @@ class UserOnlyWidget extends StatsOverviewWidget
         return [
             //
             Stat::make('Number of Users', User::query()->withTrashed()->count())->description('All Users registered on this Application')->icon('heroicon-o-user-group'),
-            Stat::make('Verified Users', User::query()->withTrashed()->whereNotNull('email_verified_at')->count())->description('All Users with Verified Accounts')->icon('clarity-employee-group-line'),
+            Stat::make('Verified Users', User::query()->withTrashed()->whereNotNull('email_verified_at')->count())
+                ->description('All Users with Verified Accounts')->icon('lineawesome-user-check-solid'),
             Stat::make('Deleted Users', User::onlyTrashed()->count())
                 ->description('Number of users who have been soft deleted')
                 ->icon('forkawesome-user-times'),
-            Stat::make('Employees On Leave', Leave::query()->where('status', '=', 'active')->count())->description('All Employees On Leave')->icon('pepicon-leave-circle-off')
+            Stat::make('Employed Users', User::query()->whereHas('employees')->count())->description('All Users Employed')->icon('pepicon-leave-circle-off')
         ];
     }
 }
